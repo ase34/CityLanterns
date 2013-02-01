978117562e4bbe026b63ed5616ce56bd41079dbb
ase34 <dev@wuemc.de>
2013-02-01 17:11:32 +0100

    Changed lantern save world identifier: Instead of saving the world name, now the uid of the world is used

--- src/main/java/me/ase34/citylanterns/storage/LanternFileStorage.java
+++ src/main/java/me/ase34/citylanterns/storage/LanternFileStorage.java
@@ -6,7 +6,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -23,30 +26,56 @@ public class LanternFileStorage implements LanternStorage {
     public void save(List<Location> lanterns) throws Exception {
         FileOutputStream fos = new FileOutputStream(file);
         DataOutputStream dos = new DataOutputStream(fos);
-        for (Location loc : lanterns) {
-            dos.writeInt(loc.getWorld().getName().getBytes().length);
-            dos.write(loc.getWorld().getName().getBytes());
-            dos.writeInt(loc.getBlockX());
-            dos.writeInt(loc.getBlockY());
-            dos.writeInt(loc.getBlockZ());
+        
+        Map<UUID, List<Location>> map = orderByWorld(lanterns);
+        
+        for (UUID uid: map.keySet()) {
+            dos.writeLong(uid.getMostSignificantBits());
+            dos.writeLong(uid.getLeastSignificantBits());
+            dos.writeInt(map.get(uid).size());
+            for (Location loc: map.get(uid)) {
+                dos.writeInt(loc.getBlockX());
+                dos.writeInt(loc.getBlockY());
+                dos.writeInt(loc.getBlockZ());
+            }
         }
+        
         dos.close();
         fos.close();
+    }
+    
+    private Map<UUID, List<Location>> orderByWorld(List<Location> locs) {
+        Map<UUID, List<Location>> map = new HashMap<UUID, List<Location>>();
+        for (Location loc: locs) {
+            UUID uid = loc.getWorld().getUID();
+            if (!map.containsKey(uid)) {
+                map.put(uid, new ArrayList<Location>());
+            }
+            map.get(uid).add(loc);
+        }
+        return map;
     }
 
     public List<Location> load() throws Exception {
         List<Location> lanterns = new ArrayList<Location>();
         FileInputStream fis = new FileInputStream(file);
         DataInputStream dis = new DataInputStream(fis);
+        
         while (dis.available() > 0) {
-            byte[] worldname = new byte[dis.readInt()];
-            dis.read(worldname);
-            World world = Bukkit.getWorld(new String(worldname));
-            int x = dis.readInt();
-            int y = dis.readInt();
-            int z = dis.readInt();
-            lanterns.add(new Location(world, x, y, z));
+            long msbs = dis.readLong();
+            long lsbs = dis.readLong();
+            UUID uid = new UUID(msbs, lsbs);
+            int size = dis.readInt();
+            World world = Bukkit.getWorld(uid);
+            
+            for (int i = 0; i < size; i++) {
+                int x = dis.readInt();
+                int y = dis.readInt();
+                int z = dis.readInt();
+                lanterns.add(new Location(world, x, y, z));
+            }
         }
+        
         dis.close();
         fis.close();
         return lanterns;
